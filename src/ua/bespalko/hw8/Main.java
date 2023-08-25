3e7daf1857d178e4fdf4d4aee9fccacee4cf4e3b
Anastasiia-Bespalko <bespalko14032000@gmail.com>
2023-08-25 10:01:48 +0300

    add hw8

--- src/ua/bespalko/hw8/Main.java
+++ src/ua/bespalko/hw8/Main.java
@@ -13,22 +13,3 @@ interface Pow {
     int pow(int number, int pow);
 }
 
-public class Main {
-    public static void main(String[] args) {
-        // Реализация функционального интерфейса Pow через лямбда-выражение
-        Pow powFunction = (number, pow) -> {
-            int result = 1;
-            for (int i = 0; i < pow; i++) {
-                result *= number;
-            }
-            return result;
-        };
-
-        // Пример использования
-        int base = 2;
-        int exponent = 5;
-        int result = powFunction.pow(base, exponent);
-        System.out.println(base + " в степени " + exponent + " = " + result);
-    }
-}
-
