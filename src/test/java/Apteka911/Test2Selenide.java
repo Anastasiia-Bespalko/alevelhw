03fe947686cccecb0913477586486d42366f6f1f
Anastasiia-Bespalko <bespalko14032000@gmail.com>
2023-09-18 15:31:38 +0300

    remove test2 selenide

--- src/test/java/Apteka911/Test2Selenide.java
+++ src/test/java/Apteka911/Test2Selenide.java
@@ -2,6 +2,7 @@ package Apteka911;
 
 import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Selenide;
+import com.codeborne.selenide.SelenideElement;
 import com.codeborne.selenide.WebDriverRunner;
 
 import static com.codeborne.selenide.Selenide.$x;
@@ -10,16 +11,18 @@ public class Test2Selenide {
     public static void main(String[] args) {
         Selenide.open("https://apteka911.ua/ua");
         WebDriverRunner.getWebDriver().manage().window().maximize();
-        $x("//a[@class='personal-area__user-name']").click();
-        $x("//a[@class='personal-area__user-login']").click();
-        $x("//input[@type='text'][@name='login']").setValue("bespalko140300@gmail.com");
-        $x("//input[@name='password']").setValue("password");
-        $x("//input[@class='btn btn-accent btn-m btn_mw-180']").click();
-        $x("//a[@class='personal-area__user-name']").shouldBe(Condition.visible);
+        SelenideElement input = Selenide.$x("//a[@class='personal-area__user-name']");
+        input.click();
+        SelenideElement login = Selenide.$x("//a[@class='personal-area__user-login']");
+        login.click();
+        SelenideElement emainOrNumber = Selenide.$x("//input[@type='text'][@name='login']").setValue("bespalko140300@gmail.com");
+        SelenideElement password = Selenide.$x("//input[@name='password']").setValue("password");
+        SelenideElement entryButton = Selenide.$x("//input[@class='btn btn-accent btn-m btn_mw-180']");
+        entryButton.click();
+        SelenideElement successfulLogin = Selenide.$x("//a[@class='personal-area__user-name']").shouldBe(Condition.visible);
 
         assert $x("//a[@class='personal-area__user-name']").isDisplayed() : "Элемент не отображается после входа";
 
         Selenide.closeWindow();
-
     }
 }
