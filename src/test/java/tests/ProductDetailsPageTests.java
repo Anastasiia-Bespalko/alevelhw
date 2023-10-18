c27604f334284f30be5d04766691c5acdcb29505
Anastasiia-Bespalko <bespalko14032000@gmail.com>
2023-10-18 19:20:35 +0300

    allureHW

--- src/test/java/tests/ProductDetailsPageTests.java
+++ src/test/java/tests/ProductDetailsPageTests.java
@@ -1,7 +1,6 @@
 package tests;
 
 import org.testng.Assert;
-import org.testng.annotations.Listeners;
 import org.testng.annotations.Test;
 import page_objects.HomePage;
 import page_objects.ProductDetailsPage;
